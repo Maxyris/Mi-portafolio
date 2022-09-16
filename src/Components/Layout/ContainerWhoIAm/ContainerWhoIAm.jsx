1b43e05da1e9bc356ca2af20ad0ac6e7c17a9d03
Dev-Maxyk <bsosorio550@misena.edu.co>
2022-09-16 14:49:39 -0500

    Se terminó el diseño basico de la información personal junto con una foto

--- src/Components/Layout/ContainerWhoIAm/ContainerWhoIAm.jsx
+++ src/Components/Layout/ContainerWhoIAm/ContainerWhoIAm.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
 import './ContainerWhoIAm.css'
 import { Text } from '../../UI/Text/Text'
+import { MyImage } from '../../UI/MyImage/MyImage'
+
 
 
 export const ContainerWhoIAm = () => {
   return (
-    <div className='ContainerTextWhoiAm'>
+    <div className='containerWhoiAm'>
+      <div className="containerTextWhoIam">
         <Text text="Mi nombre es Bryan Steven Osorio Zuleta soy tecnológo en Analisis y desarrollo de sistemas de información
-        del SENA Galan de la ciudad de Armenia-Quindio"/>
+          del SENA Galan de la ciudad de Armenia-Quindio, tengo 19 años y soy una persona seria." TextMainClassName="textClassName"/>
+      </div>
+      <div className="containerImage">
+        <MyImage/>
+      </div>
     </div>
   )
 }
