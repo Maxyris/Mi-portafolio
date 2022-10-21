2f3a7282045c6cf3c9643a511ac5bf9b987c6c3e
Dev-Maxyk <bsosorio550@misena.edu.co>
2022-10-21 12:13:10 -0500

    Se agregó las referencias personales usando datatable el cual se muestra en la pagina principal

---
+++ src/Components/Layout/PersonalReference/PersonalReference.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import DataTable from 'react-data-table-component'
+import 'styled-components'
+import './PersonalReference.css'
+
+const PersonalReferenceTable = [
+    {id:1, nombre:"Arle Morales Ortiz", cargo:"Instructor SENA y Desarrollador Software",telefono:"3137082781",correo:"amorales512@misena.edu.co"},
+    {id:2, nombre:"Juan David Romero Rendón", cargo:"Operario",telefono:"3207480490",correo:"romerojuandavid519@gmail.com"}
+]
+
+const columnas = [
+    {
+        name: 'Nombre',
+        selector: 'nombre',
+        sortable: true,
+        
+    },
+    {
+        name: 'Cargo',
+        selector: 'cargo',
+        sortable: true
+    },
+    {
+        name: 'Telefono',
+        selector: 'telefono',
+        sortable: true
+    },
+    {
+        name: 'Correo',
+        selector: 'correo',
+        sortable: true
+    }
+]
+
+
+export const PersonalReference = () => {
+  return (
+    <div className='datatablePersonalReference'>
+        <DataTable
+        columns={columnas}
+        data={PersonalReferenceTable}
+        title="Referencias personales"  
+        />
+    </div>
+  )
+}
--- src/Components/Layout/WorkExperience/WorkExperience.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const WorkExperience = () => {
-  return (
-    <div>
-        
-    </div>
-  )
-}
